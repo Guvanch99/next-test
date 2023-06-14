3e6158be845124e60e1334e04c05e6469b3ee708
Guvanch Avediyev <guvanch.avediyev@exposit.com>
2023-06-14 16:26:21 +0300

    initial nextjs

---
+++ my-app/core/styles/GlobalStyles.js
@@ -0,0 +1,56 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  ::-webkit-scrollbar {
+    background-color: transparent;
+    width: 8px
+  }
+
+  /* background of the scrollbar except button or resizer */
+  ::-webkit-scrollbar-track {
+    background-color: transparent
+  }
+
+  ::-webkit-scrollbar-track:hover {
+    background-color: transparent
+  }
+
+  /* scrollbar itself */
+  ::-webkit-scrollbar-thumb {
+    background-color: #424040;
+    border-radius: 16px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #424040;
+  }
+
+  /* set button(top and bottom of the scrollbar) */
+  ::-webkit-scrollbar-button {
+    display: none
+  }
+
+  html,
+  body {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    color: #fff;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  #__next {
+    width: 100%;
+    height: 100%;
+  }
+`
---
+++ my-app/pages/_app.js
@@ -0,0 +1,22 @@
+import {Hydrate, QueryClient, QueryClientProvider} from 'react-query';
+import { ThemeProvider} from 'styled-components';
+import {GlobalStyle} from "../core/styles/GlobalStyles";
+import {styledTheme} from "../core/styles/styled";
+
+
+function MyApp({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={styledTheme}>
+        <GlobalStyle/>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
